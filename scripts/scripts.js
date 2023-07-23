385232381bc5f9e633a1d7b08f8906422b9dfb0c
duytamdev <duytam.dev@gmail.com>
2023-07-23 08:35:53 +0700

    update ui, fix bug

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,35 @@
 const infoContainer = document.getElementById('section-info-detail');
 const btnSubmit = document.querySelector('.btn-submit');
 const formInfo = document.getElementById('form-info');
+const emailInput = document.getElementById('emailInput');
+const btnCertificate= document.getElementById('btn-certificate');
+const subInput = document.getElementById('sub-input');
+const moreBtn = document.querySelector('.more-btn');
+const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+let isCollapse= false;
+
+moreBtn.addEventListener('click', (e) => {
+    moreBtn.classList.toggle('more-btn--active');
+});
 
 btnSubmit.addEventListener('click', (e) => {
     // remove class
-    infoContainer.classList.remove('hide');
-    formInfo.classList.add('hide');
+    if(emailInput.value.match(regexEmail)) {
+        infoContainer.classList.remove('hide');
+        formInfo.classList.add('hide');
+    }else{
+        subInput.classList.add('sub-input--danger');
+        subInput.innerHTML = 'Email không hợp lệ, vui lòng nhập lại';
+    }
+
 });
+btnCertificate.addEventListener('click', (e) => {
+    scrollToSection('certificate');
+});
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
